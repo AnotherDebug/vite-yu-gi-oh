977db9e6aafe931a7df0871b4fcd552b55e4cdfe
AnotherDebug <antonio.faddanno@gmail.com>
2023-10-12 17:37:01 +0200

    [ADD]: markup e style img

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,25 +1,32 @@
 <script>
 export default {
-name: 'Header'
+  name: 'Header'
 }
 </script>
 
 
 <template>
-
   <header>
-    <div class="container my-3">
-      <h1>Yu-Gi-Oh Api</h1>
-    </div>
-  </header>
 
+    <img src="../../public/yugioh_logo__2016__by_alanmac95_d92o1t0-375w-2x.png" alt="">
+
+  <h1>Yu-Gi-Oh Api</h1>
+
+  </header>
 </template>
 
 
 <style lang="scss" scoped>
-
-header{
+header {
+  display: flex;
+  align-items: center;
   height: 80px;
+  margin-left: 20px;
+  img {
+    height: 25%;
+  }
+  h1 {
+    margin-left: 10px;
+  }
 }
-
 </style>
