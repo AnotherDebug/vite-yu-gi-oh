ec3febb2f01d7645ead8e07f74ae3108b14692cf
AnotherDebug <antonio.faddanno@gmail.com>
2023-10-12 16:20:28 +0200

    [ADD]: import store e axios method getApi

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,39 @@
 <script>
 import Header from './components/Header.vue';
 import Main from './components/Main.vue';
+import { store } from './data/store';
+import axios from 'axios';
 export default {
 name: "App",
 components: {
   Header,
   Main
+},
+data() {
+  return{
+    store
+  }
+},
+methods:{
+  getApi() {
+    axios.get(store.apiUrl)
+    .then(res => {
+      store.cardsList = res.data;
+      console.log(store.cardsList);
+    })
+    .catch(e => {
+      console.log(e);
+    })
+  }
+},
+mounted() {
+  this.getApi();
 }
 }
 </script>
 
 
 <template>
-  
-<h1>Yu-Gi-Oh</h1>
 
 <Header />
 <Main />
