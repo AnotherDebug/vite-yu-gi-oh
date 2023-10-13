40144e6c38055d6da6c3ce5d68d6338d61c98c23
AnotherDebug <antonio.faddanno@gmail.com>
2023-10-13 08:09:00 +0200

    [ADD]: FoundCards component

---
+++ src/components/partials/FoundCards.vue
@@ -0,0 +1,41 @@
+<script>
+import { store } from '../../data/store';
+export default {
+    name: 'FoundCards',
+    data() {
+        return {
+            store
+        }
+    },
+    computed:{
+            
+            foundCards() {
+                if(store.cardsList.length > 0){
+                    return `Found ${store.cardsList.length} cards`;
+                }else {
+                    return "No cards found";
+                }
+            }
+        }
+}
+</script>
+
+
+<template>
+
+    <div class="found">
+        <h5>{{ foundCards }}</h5>
+    </div>
+
+</template>
+
+
+<style lang="scss" scoped>
+
+@use '../../scss/partials/variables' as *;
+ .found {
+            background-color: $black-color;
+            color: $white-color;
+            padding: 10px;
+        }
+</style>
